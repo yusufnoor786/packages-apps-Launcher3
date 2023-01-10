7073e496985617400f3079a9b2d12dfd9713637b
Joker-V2 <nipinna0@gmail.com>
2023-01-10 02:57:55 +0000

    Launcher3: Revert "restart model on hidden apps list change"
    - No longer needed , Hidden apps feature has been merged with AppLock
    
    This reverts commit c8163752de06eaa87c4b4679bf14fe78f23ec4aa.
    
    Signed-off-by: Anush02198 <anush.4376@gmail.com>

--- src/com/android/launcher3/LauncherAppState.java
+++ src/com/android/launcher3/LauncherAppState.java
@@ -28,10 +28,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.pm.LauncherApps;
-import android.database.ContentObserver;
-import android.net.Uri;
 import android.os.UserHandle;
-import android.provider.Settings;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
@@ -65,7 +62,6 @@ public class LauncherAppState implements SafeCloseable {
             new MainThreadInitializedObject<>(LauncherAppState::new);
 
     private final Context mContext;
-    private final ContentObserver mSettingsObserver;
     private final LauncherModel mModel;
     private final ThirdPartyIconProvider mIconProvider;
     private final IconCache mIconCache;
@@ -177,18 +173,6 @@ public class LauncherAppState implements SafeCloseable {
         mModel = new LauncherModel(context, this, mIconCache, new AppFilter(mContext),
                 iconCacheFileName != null);
         mOnTerminateCallback.add(mIconCache::close);
-        mSettingsObserver = new ContentObserver(null) {
-            @Override
-            public void onChange(boolean selfChange, Uri uri) {
-                final String key = uri.getLastPathSegment();
-                if (key.equals(Settings.Secure.LAUNCHER_HIDDEN_APPS)) {
-                    refreshAndReloadLauncher();
-                }
-            }
-        };
-        mContext.getContentResolver().registerContentObserver(
-            Settings.Secure.getUriFor(Settings.Secure.LAUNCHER_HIDDEN_APPS),
-            false /* notifyForDescendants */, mSettingsObserver);
     }
 
     private void onNotificationSettingsChanged(boolean areNotificationDotsEnabled) {
@@ -210,7 +194,6 @@ public class LauncherAppState implements SafeCloseable {
      */
     @Override
     public void close() {
-        mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
         mModel.destroy();
         mContext.getSystemService(LauncherApps.class).unregisterCallback(mModel);
         CustomWidgetManager.INSTANCE.get(mContext).setWidgetRefreshCallback(null);
