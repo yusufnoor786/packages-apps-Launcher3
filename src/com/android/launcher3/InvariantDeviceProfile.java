AICP/packages_apps_Launcher3

Launcher3: Allow disabling workspace edit
Change-Id: I503e19cbc512eac0e4a8c8bccc16a6ccc0e805da
Signed-off-by: Joey <joey@lineageos.org>

src/com/android/launcher3/InvariantDeviceProfile.java
@@ -75,6 +75,7 @@ public class InvariantDeviceProfile {
     public static final int CHANGE_FLAG_ICON_PARAMS = 1 << 1;
 
     public static final String KEY_ICON_PATH_REF = "pref_icon_shape_path";
+    public static final String KEY_WORKSPACE_EDIT = "pref_workspace_edit";
 
     // Constants that affects the interpolation curve between statically defined device profile
     // buckets.

src/com/android/launcher3/views/OptionsPopupView.java
@@ -159,7 +159,7 @@ public class OptionsPopupView extends ArrowPopup
                 R.drawable.ic_palette : R.drawable.ic_wallpaper;
         options.add(new OptionItem(resString, resDrawable,
                 ControlType.WALLPAPER_BUTTON, OptionsPopupView::startWallpaperPicker));
-        if (!FeatureFlags.GO_DISABLE_WIDGETS) {
+        if (!FeatureFlags.GO_DISABLE_WIDGETS && Utilities.isWorkspaceEditAllowed(launcher)) {
             options.add(new OptionItem(R.string.widget_button_text, R.drawable.ic_widget,
                     ControlType.WIDGETS_BUTTON, OptionsPopupView::onWidgetsClicked));
         }

yusufnoor786/packages-apps-Launcher3

Co-authored-by: Michael W <baddaemon87@gmail.com>
Co-authored-by: Han Wang <416810799@qq.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Anush02198 <anush.4376@gmail.com>

src/com/android/launcher3/InvariantDeviceProfile.java
@@ -90,6 +90,8 @@ public class InvariantDeviceProfile {
 
     private static final float ICON_SIZE_DEFINED_IN_APP_DP = 48;
 
+    public static final String KEY_WORKSPACE_LOCK = "pref_workspace_lock";
+
     // Constants that affects the interpolation curve between statically defined device profile
     // buckets.
     private static final float KNEARESTNEIGHBOR = 3;

src/com/android/launcher3/views/OptionsPopupView.java
@@ -189,7 +189,7 @@ public class OptionsPopupView extends ArrowPopup<Launcher>
                 resDrawable,
                 IGNORE,
                 OptionsPopupView::startWallpaperPicker));
-        if (!WidgetsModel.GO_DISABLE_WIDGETS) {
+        if (!WidgetsModel.GO_DISABLE_WIDGETS && Utilities.isWorkspaceEditAllowed(launcher)) {
             options.add(new OptionItem(launcher,
                     R.string.widget_button_text,
                     R.drawable.ic_widget,